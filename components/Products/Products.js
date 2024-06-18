1e5287b41f026da5f1bdb90530305e4d2fee1076
azizjohnny <xnarxuz@gmail.com>
2024-06-18 13:47:39 -0700

    Update Products.js

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -11,7 +11,7 @@ import { useEffect } from "react";
 import axios from "axios";
 import { Spinner } from "../Spinner/Spinner";
 
-const env = process.env.NEXT_PUBLIC_TOKEN;
+const env = http://194.31.52.65:8080/api/;
 
 export default function Products({ data }) {
   // const searchParams = useSearchParams();
